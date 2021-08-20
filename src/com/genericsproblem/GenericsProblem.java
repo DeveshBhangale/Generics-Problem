36f2052eddd8e8183c76571d321bfe307379abc1
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-20 14:06:37 +0530

    [Devesh]: welcome message

---
+++ src/com/genericsproblem/GenericsProblem.java
@@ -0,0 +1,14 @@
+package com.genericsproblem;
+
+public class GenericsProblem {
+
+	public GenericsProblem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
