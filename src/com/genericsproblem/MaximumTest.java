55454effb363168cd6a563ba067f11b3a31f036c
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-20 14:32:31 +0530

    [Devesh]: added  compareTo method for float type

--- src/com/genericsproblem/MaximumTest.java
+++ src/com/genericsproblem/MaximumTest.java
@@ -6,7 +6,24 @@ public class MaximumTest {
 	}
 
 	public static void main(String[] args) {
-		Integer a=10,b=11,c=9;
+		//Use Case - 1 
+//		Integer a=10,b=11,c=9;
+//		int res = a.compareTo(b);
+//		if(res==-1 || res == 0) {
+//			int res1 = b.compareTo(c);
+//			if(res1 == -1) {
+//				System.out.println(c +" is maximum than "+b+" & "+a);
+//			}
+//			else if(res1 == 0) {
+//				System.out.println("All three Integers are equal");
+//			}
+//			else {
+//				System.out.println(b +" is maximum than "+c+" & "+a);
+//			}
+//		}
+		
+		// Use Case - 2
+		Float a =  1.1f, b= 1.1f, c = 1.1f;
 		int res = a.compareTo(b);
 		if(res==-1 || res == 0) {
 			int res1 = b.compareTo(c);
@@ -20,8 +37,6 @@ public class MaximumTest {
 				System.out.println(b +" is maximum than "+c+" & "+a);
 			}
 		}
-			
-
 	}
 
 }
